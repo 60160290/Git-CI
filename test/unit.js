a5e61810a4a9808558bb554ae320ddd0bcb1ec0b
60160290 <60160290@go.buu.ac.th>
2020-11-13 12:54:36 +0700

    unit test

---
+++ test/unit.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+const request = require('supertest')
+const app = require('../app')
+
+
+describe('GET "/items"', () =>{
+    it('respond with hello!', (done) =>{
+        let want = [
+            { id : 1, name:'iPhone12 Pro Max'},
+            { id: 2, name:'Google Pixel 5'}
+        ]
+
+        request(app).get('/items').expect(want, done)
+    })
+})
